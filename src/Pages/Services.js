0e13a68cd40475da588a6e097f5eb5ad9c5668ea
malith2cristal <malith.gayashan@cristalcode.net>
2023-02-13 20:09:18 +0530

    finished service page

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import Cards2 from '../componets/cards2';
 import Nav from '../componets/Nav';
+import { ButtonGroup,Divider,Stack,Image,Heading,Text,Button,Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
+
 
 function Services() {
   return (
@@ -12,6 +15,15 @@ function Services() {
       <h2 className="topic8">“Great hair doesn’t happen <br></br>
       by chance, it happens by<br></br>
       appointment“</h2>
+
+      <Cards2 />
+
+      <h2 className="topic10">I’ll capture your best side at <br></br>
+      your next photoshoot.</h2>
+
+      <Button className="BtnCard2">
+            <a className="textcolor">Work With Me</a>
+          </Button>
 
 
 
